d7e5a66d5384cc901b8ecc0b8e04771ad2d33b32
serping <seadfeng@gmail.com>
2024-07-08 22:55:01 +0800

    update ItemSource

--- components/frontend/google/shared/ItemSource.tsx
+++ components/frontend/google/shared/ItemSource.tsx
@@ -15,7 +15,7 @@ export function ItemSource({
   return( 
     <div className={cn(className,"relative flex items-center space-x-3 pb-3 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400")}>
       <div className="flex-shrink-0">
-         <img src={`https://www.google.com/s2/favicons?domain=${url.hostname}&sz=${size}`} className="h-10 w-10 rounded-full" />
+         <img src={`https://www.google.com/s2/favicons?domain=${url.hostname}&sz=${size}`} className="h-8 w-8 rounded-full" />
       </div>
       <div className="min-w-0 flex-1">
         <a href={source.link} className="focus:outline-none">
