0a01592f27fd9b0ccc5e4774f05cdbc5e3b36e45
serping <seadfeng@icloud.com>
2024-07-12 01:14:05 +0800

    update ui color

--- components/frontend/shared/header.tsx
+++ components/frontend/shared/header.tsx
@@ -29,7 +29,7 @@ export function Header() {
   }, [debouncedScroll]);
 
   return(
-  <header className={cn("fixed left-0 top-0 w-full items-center gap-4 border-b bg-background px-4 md:px-6 z-50 h-16 transition-all duration-200 opacity-100", isTop ? "shadow-none flex" : "shadow-sm hidden opacity-0")}>
+  <header className={cn(" w-full items-center gap-4 border-b bg-background px-4 md:px-6 z-50 h-16 transition-shadow duration-200 flex", isTop ? "shadow-none" : "shadow-sm")}>
     <NavBar />
     <div className="flex items-center justify-end gap-4 md:ml-auto md:gap-2 lg:gap-4">
       <LocaleSwitch />
