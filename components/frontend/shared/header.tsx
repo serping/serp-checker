88b6c93829fd7179071e3b9ecf14c7e4d2696a33
serping <seadfeng@icloud.com>
2024-07-12 01:19:33 +0800

    update header

--- components/frontend/shared/header.tsx
+++ components/frontend/shared/header.tsx
@@ -31,7 +31,7 @@ export function Header() {
   return(
   <header className={cn(" w-full items-center gap-4 border-b bg-background px-4 md:px-6 z-50 h-16 transition-shadow duration-200 flex", isTop ? "shadow-none" : "shadow-sm")}>
     <NavBar />
-    <div className="flex items-center justify-end gap-4 md:ml-auto md:gap-2 lg:gap-4">
+    <div className="flex items-center justify-end gap-2 md:ml-auto text-primary">
       <LocaleSwitch />
       <ModeToggle />
     </div>
