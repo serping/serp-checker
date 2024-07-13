22e76046d40415e94f05a938f7ddd311b4d6f52a
serping <seadfeng@icloud.com>
2024-07-13 11:22:17 +0800

    update footer

--- components/frontend/shared/footer.tsx
+++ components/frontend/shared/footer.tsx
@@ -2,7 +2,7 @@
 
 export function Footer() {
   return (
-    <footer className="container py-4 text-center text-sm text-muted-foreground">
+    <footer className="container py-4 text-center text-sm text-muted-foreground mt-10">
       <span>
         © {new Date().getFullYear()} {appConfig.appName}. All rights reserved.
       </span>
