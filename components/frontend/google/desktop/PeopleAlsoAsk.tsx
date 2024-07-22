10c96d82dda937867e9d54dbbabf97553bc02662
serping <seadfeng@icloud.com>
2024-07-22 12:50:12 +0800

    update PeopleAlsoAsk

--- components/frontend/google/desktop/PeopleAlsoAsk.tsx
+++ components/frontend/google/desktop/PeopleAlsoAsk.tsx
@@ -17,7 +17,7 @@ export function PeopleAlsoAsk({original, className}:{original: SerpPeopleAlsoAsk
           <li key={`people_also_ask-${index}`} className="flex space-x-6 py-2">   
             <div className="flex-auto space-y-1 max-w-[200px] lg:max-w-[300px]">
               <h3 className="truncate flex items-center">
-                {item.question} <a href={item.source.link} target="_blank" title={item.question}> <ExternalLinkIcon size={15} className="ml-2" /></a>
+                {item.question} {item.source &&<a href={item.source.link} target="_blank" title={item.question}> <ExternalLinkIcon size={15} className="ml-2" /></a>}
               </h3>  
             </div>
           </li> 
