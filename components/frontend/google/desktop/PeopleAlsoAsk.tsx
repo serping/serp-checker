c5ce1c29b08ff9e6df335b17b7b4e5b3e264257b
serping <seadfeng@icloud.com>
2024-07-26 16:49:40 +0800

    update PeopleAlsoAsk

--- components/frontend/google/desktop/PeopleAlsoAsk.tsx
+++ components/frontend/google/desktop/PeopleAlsoAsk.tsx
@@ -3,6 +3,7 @@
 import { cn } from "@/lib/utils";
 import { ExternalLinkIcon } from "lucide-react";
 import { useTranslations } from "next-intl";
+import Link from "next/link";
 import type { SerpPeopleAlsoAsk } from "serping/zod/google/desktop-serp";
 import { TypeTitle } from "../shared/TypeTitle";
 
@@ -15,10 +16,11 @@ export function PeopleAlsoAsk({original, className}:{original: SerpPeopleAlsoAsk
       <ul role="list" className="divide-y divide-gray-200 text-sm font-medium text-muted-foreground">
         {original.people_also_ask.map((item, index) =>  
           <li key={`people_also_ask-${index}`} className="flex space-x-6 py-2">   
-            <div className="flex-auto space-y-1 max-w-[200px] lg:max-w-[300px]">
-              <h3 className="truncate flex items-center">
-                {item.question} {item.source &&<a href={item.source.link} target="_blank" title={item.question}> <ExternalLinkIcon size={15} className="ml-2" /></a>}
-              </h3>  
+            <div className="flex-auto space-y-1">
+              <h3 className="truncate flex items-center text-blue-700">
+                {item.question} {item.source && <a href={item.source.link} target="_blank" title={item.question}> <ExternalLinkIcon size={15} className="ml-2" /></a>}
+              </h3>
+              {item.source && <Link href={item.source.link}>{item.source.link.replace(/#.*/,'')}</Link>} 
             </div>
           </li> 
         )}
