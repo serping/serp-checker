925be168581ca19366ab349a3d38aeef910c5cef
serping <seadfeng@icloud.com>
2024-10-14 17:02:51 +0800

    update SiteIcon

--- components/frontend/google/shared/SiteIcon.tsx
+++ components/frontend/google/shared/SiteIcon.tsx
@@ -3,13 +3,11 @@
 import { cn } from "@/lib/utils";
 export function SiteIcon({
   link,
-  size = 50,
   className
 }:{
   link: string;
-  size?: number; 
   className?: string;
 }){
   const url = new URL(link);
-  return <img src={`https://www.google.com/s2/favicons?domain=${url.hostname}&sz=${size}`} className={cn("h-8 w-8 rounded-full", className)} />
+  return <img src={`https://www.faviconextractor.com/favicon/${url.hostname}`} className={cn("h-8 w-8 rounded-full", className)} />
 }
